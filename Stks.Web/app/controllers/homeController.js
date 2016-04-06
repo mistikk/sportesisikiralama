d90769afbea5bee88ef8bbf6935711e6dbb25fdc
Mustafa Büyükçelebi <mustafa.buyukcel@gmail.com>
2016-04-06 14:48:27 +0300

    datepicker eklendi

--- Stks.Web/app/controllers/homeController.js
+++ Stks.Web/app/controllers/homeController.js
@@ -1,4 +1,18 @@
 ﻿'use strict';
 app.controller('homeController', ['$scope', function ($scope) {
+
+    $scope.today = "06/04/2016";
+
+    $('.input-daterange input').each(function () {
+        $(this).datepicker({
+            format: "dd/mm/yyyy",
+            todayBtn: "linked",
+            language: "tr",
+            todayHighlight: true,
+            autoclose: true,
+            startDate: $scope.today
+        });
+    });
+
    
 }]);
---
+++ Stks.Web/scripts/site.js
@@ -0,0 +1,4 @@
+﻿$('.datepicker').datepicker({
+    format: "dd/mm/yyyy",
+    language: "tr"
+});
